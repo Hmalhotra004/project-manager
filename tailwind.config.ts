9ff63e75ed7de9166f5e39cc4e6603f3fa153eeb
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-08-13 22:00:48 +0530

    initial commit

---
+++ tailwind.config.ts
@@ -0,0 +1,20 @@
+import type { Config } from "tailwindcss";
+
+const config: Config = {
+  content: ["./src/pages/**/*.{js,ts,jsx,tsx,mdx}", "./src/components/**/*.{js,ts,jsx,tsx,mdx}", "./src/app/**/*.{js,ts,jsx,tsx,mdx}"],
+  theme: {
+    extend: {
+      backgroundImage: {
+        "gradient-radial": "radial-gradient(var(--tw-gradient-stops))",
+        "gradient-conic": "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
+      },
+      screens: {
+        xxs: "320px",
+        xs: "425px",
+        xxl: "1536px",
+      },
+    },
+  },
+  plugins: [],
+};
+export default config;
