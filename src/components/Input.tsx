27354ca5c6d20ec79ddc25dbeb1e18f67aa38590
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-08-25 18:13:52 +0530

    New Project Screen

---
+++ src/components/Input.tsx
@@ -0,0 +1,28 @@
+type Props = {
+  textArea: boolean;
+  label: string;
+};
+
+const Input = ({ label, textArea, ...props }: Props) => {
+  const cssClass = `w-full p-1 border-b-2 rounded-sm 
+                    border-stone-300 bg-stone-200 text-stone-600 
+                    focus:outline-none focus:border-stone-600`;
+  return (
+    <p className="flex flex-col gap-1 my-4">
+      <label className="text-sm font-bold uppercase text-stone-500">{label}</label>
+      {textArea ? (
+        <textarea
+          className={cssClass}
+          {...props}
+        />
+      ) : (
+        <input
+          className={cssClass}
+          {...props}
+        />
+      )}
+    </p>
+  );
+};
+
+export default Input;
