90c598d72046aab5e4bb425fd4f057b5f9ed8b0e
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-09-29 14:18:17 +0530

    shadcn prisma init delete button in sidebar

--- src/components/ProjectsSidebar.tsx
+++ src/components/ProjectsSidebar.tsx
@@ -2,8 +2,10 @@
 import { projectActions } from "@/store/projectslice";
 import { AppDispatch } from "@/store/store";
 import { Project } from "@/types";
+import { Trash2 } from "lucide-react";
 import { useDispatch, useSelector } from "react-redux";
 import Button from "./Button";
+import { ScrollArea } from "./ui/scroll-area";
 
 const ProjectsSidebar = () => {
   const dispatch = useDispatch<AppDispatch>();
@@ -22,32 +24,38 @@ const ProjectsSidebar = () => {
   return (
     <>
       <aside className="w-1/3 px-6 pt-8 pb-4 bg-stone-900 text-stone-50 md:w-72 h-screen flex flex-col">
-        <h2 className="mb-4 font-bold uppercase md:text-xl text-stone-200">Your Tasks</h2>
+        <h2 className="mb-4 font-bold uppercase md:text-xl text-stone-200">Your Projects</h2>
         <div>
-          <Button onClick={handleAddClick}>+ Add Tasks</Button>
+          <Button onClick={handleAddClick}>+ Add Project</Button>
         </div>
-        <ul className="my-4 overflow-y-scroll">
-          {projects.map(project => {
-            let cssClass = "w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800 transition-colors";
+        <ScrollArea>
+          <ul className="my-4">
+            {projects.map(project => {
+              let cssClass = "w-full text-left px-2 py-1 rounded-sm my-1 hover:text-stone-200 hover:bg-stone-800 transition-colors";
 
-            if (project.id === selectedProjectId) {
-              cssClass += " text-stone-200 bg-stone-800";
-            } else {
-              cssClass += " text-stone-400";
-            }
+              if (project.id === selectedProjectId) {
+                cssClass += " text-stone-200 bg-stone-800";
+              } else {
+                cssClass += " text-stone-400";
+              }
 
-            return (
-              <li key={project.id}>
-                <button
-                  onClick={() => handleSelectClick(project.id)}
-                  className={cssClass}
-                >
-                  {project.title}
-                </button>
-              </li>
-            );
-          })}
-        </ul>
+              return (
+                <>
+                  <button
+                    onClick={() => handleSelectClick(project.id)}
+                    key={project.id}
+                    className={`${cssClass} flex`}
+                  >
+                    {project.title}
+                    <button className="ml-auto flex hover:text-rose-800 transition-all">
+                      <Trash2 />
+                    </button>
+                  </button>
+                </>
+              );
+            })}
+          </ul>
+        </ScrollArea>
         {/* <div className="flex items-center justify-end mt-auto">
           <button
             // onClick={handleLogOut}
--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -16,7 +16,7 @@ const Tasks = () => {
 
   return (
     <section>
-      <h2 className="text-2xl font-bold text-stone-700 mb-4">tasks</h2>
+      <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
       <NewTask />
       {selectedProjectTasks.length === 0 && <p className="text-stone-800 my-4">This project does not have any task yet.</p>}
       {selectedProjectTasks.length > 0 && (
