a46e2b355ac718f24d67fdec001248ce06419e74
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-10-13 01:12:37 +0530

    updated files

--- src/app/api/users/login/route.ts
+++ src/app/api/users/login/route.ts
@@ -1,26 +1,26 @@
-import db from "@/lib/db";
-import bcrypt from "bcryptjs";
-import { NextRequest, NextResponse } from "next/server";
+// import db from "@/lib/db";
+// import bcrypt from "bcryptjs";
+// import { NextRequest, NextResponse } from "next/server";
 
-export async function POST(req: NextRequest) {
-  try {
-    const { email, password } = await req.json();
+// export async function POST(req: NextRequest) {
+//   try {
+//     const { email, password } = await req.json();
 
-    const user = await db.users.findUnique({
-      where: {
-        email: email,
-      },
-    });
+//     const user = await db.users.findUnique({
+//       where: {
+//         email: email,
+//       },
+//     });
 
-    if (!user) return NextResponse.json({ error: "User not found" }, { status: 404 });
+//     if (!user) return NextResponse.json({ error: "User not found" }, { status: 404 });
 
-    const isMatch = await bcrypt.compare(password, user.password);
+//     const isMatch = await bcrypt.compare(password, user.password);
 
-    if (!isMatch) return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
+//     if (!isMatch) return NextResponse.json({ error: "Invalid credentials" }, { status: 401 });
 
-    return NextResponse.json({ message: "Login successful", user });
-  } catch (err) {
-    console.log("[users]", err);
-    return new NextResponse("Internal Error", { status: 500 });
-  }
-}
+//     return NextResponse.json({ message: "Login successful", user });
+//   } catch (err) {
+//     console.log("[users]", err);
+//     return new NextResponse("Internal Error", { status: 500 });
+//   }
+// }
