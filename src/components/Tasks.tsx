afd2240878b8345bdf36aa74c037bf33df360a74
Hardik Malhotra <hardikmalhotra150804@gmail.com>
2024-10-13 15:38:11 +0530

    varios loadig states

--- src/components/Tasks.tsx
+++ src/components/Tasks.tsx
@@ -8,14 +8,22 @@ import { useDispatch, useSelector } from "react-redux";
 import NewTask from "./NewTask";
 
 const Tasks = () => {
-  const [loading, setLoading] = useState(true);
+  const [delLoading, setDelLoading] = useState(false);
+  const [loading, setLoading] = useState(false);
   const tasks = useSelector((state: RootState) => state.tasks);
   const selectedProjectId = useSelector((state: RootState) => state.selectedProjectId);
   const dispatch = useDispatch<AppDispatch>();
 
   async function deleteTask(Id: string) {
-    await axios.post("/api/tasks/delete", { Id, projectId: selectedProjectId });
-    dispatch(projectActions.DeleteTask(Id));
+    try {
+      setDelLoading(true);
+      await axios.post("/api/tasks/delete", { Id, projectId: selectedProjectId });
+      dispatch(projectActions.DeleteTask(Id));
+    } catch (error) {
+      console.log(error);
+    } finally {
+      setDelLoading(false);
+    }
   }
 
   const selectedProjectTasks = tasks.filter(task => task.projectId === selectedProjectId);
@@ -33,9 +41,7 @@ const Tasks = () => {
       } catch (error) {
         console.error("Error fetching tasks:", error);
       } finally {
-        setTimeout(() => {
-          setLoading(false);
-        }, 1000); // Adjust the delay as needed
+        setLoading(false);
       }
     }
     getTasks();
@@ -45,12 +51,13 @@ const Tasks = () => {
     <section className="mb-12">
       <h2 className="text-2xl font-bold text-stone-700 mb-4">Tasks</h2>
       <NewTask />
-      {selectedProjectTasks.length === 0 ? (
-        <p className="text-stone-800 my-4">This project does not have any tasks yet.</p>
+
+      {loading ? (
+        <div className="loader" />
       ) : (
         <>
-          {loading ? (
-            <p className="text-black text-base my-4">Loading Tasks...</p>
+          {selectedProjectTasks.length === 0 ? (
+            <p className="text-stone-800 my-4">This project does not have any tasks yet.</p>
           ) : (
             <ul className="px-4 py-2 mt-8 rounded-md bg-stone-100">
               {selectedProjectTasks.map(task => {
@@ -63,6 +70,7 @@ const Tasks = () => {
                     <button
                       onClick={() => deleteTask(task.Id)}
                       className="text-stone-700 hover:text-red-500 transition-colors"
+                      disabled={delLoading}
                     >
                       Delete
                     </button>
